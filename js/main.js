6d7cef18bb2ae3ed57da430b612f75cfb1bef58d
Víctor César <victorcesarts@gmail.com>
2022-08-03 15:28:09 -0300

    First page done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var buttonStartGame = document.querySelector('.start-game')
+var buttonAddWord = document.querySelector('.add-word')
+
+buttonStartGame.addEventListener('click', function(){
+    window.location.replace('game.html')
+})
+
+buttonAddWord.addEventListener('click', function(){
+    window.location.replace('add-word.html')
+})
